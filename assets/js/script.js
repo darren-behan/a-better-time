bd85e37be1346cc50227e4f41d3e69c64665c7c1
Darren Behan <darrenbehan@hotmail.com>
2020-06-24 14:11:03 +1000

    initial setup of API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,7 +89,6 @@ $(document).ready(function () {
         "user-key": "8ad7cae02b2d6a7122357d5b80d69935",
       },
       success: function (response) {
-        console.log(response);
         var restaurants = [];
         var restaurantList = response.nearby_restaurants;
         for (var restaurant of restaurantList) {
@@ -119,8 +118,8 @@ $(document).ready(function () {
             cat: restaurantCat
           });
         }
-        console.log("the restaurants are")
-        console.log(restaurants + "the restaurants");
+        // console.log("the restaurants are")
+        // console.log(restaurants + "the restaurants");
         populateResults(restaurants, 0);
 
       }
@@ -143,7 +142,7 @@ $(document).ready(function () {
         function success(response) {
           latitude = response.lat;
           longitude = response.lon;
-          console.log("lat " + latitude + " long + :" + longitude);
+          // console.log("lat " + latitude + " long + :" + longitude);
         },
 
         function fail(data, status) {
@@ -202,110 +201,41 @@ $(document).ready(function () {
     // Local to your area in melbourne (local) ----------------------------------
     // changing stucture of query to on-success in order to limit async var assignment issues.birdLogo
 
-    var settings = {
+    $.ajax({
       "async": true,
       "crossDomain": true,
-      "url": "https://tripadvisor1.p.rapidapi.com/attractions/list-by-latlng?lunit=km&currency=USD&limit=30&distance=5&lang=en_US&longitude=" + longitude + "&latitude=" + latitude,
+      "url": "https://tripadvisor1.p.rapidapi.com/attractions/list-by-latlng?lunit=km&currency=AUD&limit=30&distance=5&lang=en_US&longitude=" + longitude + "&latitude=" + latitude,
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
         "x-rapidapi-key": "fdb9978b68mshd6275eb4a4e31a6p16d146jsn8702ceda1ca0"
+      },
+      success: function(response) {
+        console.log(response)
+
+        var randNum = Math.floor(Math.random() * 32);
+        var localName = response.data[randNum].name;
+        // var localCost = response.data[randNum].offer_group.lowest_price
+        // var localTime = response.data[randNum].hours.week_ranges[1][0];
+        var localDesc = response.data[randNum].description;
+        var localUrl = response.data[randNum].web_url;
+        var localImag = response.data[randNum].photo.images.small.url;
+        var localCate = response.data[randNum].subcategory[0].name;
+
+        // Sometimes the call fails due to not all the returns being the same. If the
+        // call exists we should allow it, otherwise, do not attempt return and value = nil, 
+        // nil value will equal lowest price range and return in both day and night filters.
+
+        console.log(localName);
+        // console.log(localCost);
+        // console.log(localTime);
+        console.log(localDesc);
+        console.log(localUrl);
+        console.log(localImag);
+        console.log(localCate);
       }
-    }
-
-    $.ajax(settings).done(function (response) {
-      console.log("I am a random activity local to the user");
-
-      console.log(response)
-
-      var randNum = Math.floor(Math.random() * 32);
-      var localName = response.data[randNum].name;
-      // var localCost = response.data[randNum].offer_group.lowest_price
-      // var localTime = response.data[randNum].hours.week_ranges[1][0];
-      var localDesc = response.data[randNum].description;
-      var localUrl = response.data[randNum].web_url;
-      var localImag = response.data[randNum].photo.images.small.url;
-      var localCate = response.data[randNum].subcategory[0].name;
-
-      // Sometimes the call fails due to not all the returns being the same. If the
-      // call exists we should allow it, otherwise, do not attempt return and value = nil, 
-      // nil value will equal lowest price range and return in both day and night filters.
-
-      console.log(localName);
-      // console.log(localCost);
-      // console.log(localTime);
-      console.log(localDesc);
-      console.log(localUrl);
-      console.log(localImag);
-      console.log(localCate);
-    });
-
-    // activities out to melbourne area (near) ----------------------------------
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://tripadvisor1.p.rapidapi.com/attractions/list?lang=en_US&currency=USD&sort=recommended&lunit=km&location_id=255099",
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-        "x-rapidapi-key": "fdb9978b68mshd6275eb4a4e31a6p16d146jsn8702ceda1ca0"
-      }
-    }
-
-    $.ajax(settings).done(function (response) {
-      console.log("I am a random activity up to melbourne")
-
-      // var randNum = Math.floor(Math.random() * 32);
-      // var nearName = response[randNum].name;
-      // var nearCost = response[randNum].offer_group.lowest_price;
-      // var nearTime = response.hours.week_ranges;
-      // var nearDesc = response[randNum].description;
-      // var nearUrl = response[randNum].website
-      // var nearImag = response[randNum].photo.images.small.url;
-      // var nearCate = response[randNum].category.name
-
-      // console.log(nearName);
-      // console.log(nearCost);
-      // console.log(nearTime);
-      // console.log(nearDesc);
-      // console.log(nearUrl);
-      // console.log(nearImag);
-      // console.log(nearCate);
-    });
-
-    // activities out to greater melbourne (near) ----------------------------------
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://tripadvisor1.p.rapidapi.com/attractions/list?lang=en_US&currency=USD&sort=recommended&lunit=km&location_id=2699946",
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-        "x-rapidapi-key": "fdb9978b68mshd6275eb4a4e31a6p16d146jsn8702ceda1ca0"
-      }
-    }
-
-    $.ajax(settings).done(function (response) {
-      console.log("I am a random activity up to the greater melbourne")
-
-      // var randNum = Math.floor(Math.random() * 32);
-      // var farName = response[randNum].name;
-      // var farCost = response[randNum].offer_group.lowest_price;
-      // var farTime = response.hours.week_ranges;
-      // var farDesc = response[randNum].description;
-      // var farUrl = response[randNum].website
-      // var farImag = response[randNum].photo.images.small.url;
-      // var farCate = response[randNum].category.name
-
-      // console.log(farName);
-      // console.log(farCost);
-      // console.log(farTime);
-      // console.log(farDesc);
-      // console.log(farUrl);
-      // console.log(farImag);
-      // console.log(farCate);
-    });
-  };
+    })
+  }
 
   // Davids Code ----------------------------------
   function returnRandom(number) {
@@ -322,9 +252,9 @@ $(document).ready(function () {
     if (source === 0) {
 
       zomatoAPI = populateThis;
-      console.log(zomatoAPI);
+      // console.log(zomatoAPI);
       var useThis = returnRandom(populateThis.length);
-      console.log("record to use" + useThis);
+      // console.log("record to use" + useThis);
       var addThis = populateThis[useThis];
       // each function will be like the above - however one piece of code
       // the results will populate into the input box. with the values of the keys
@@ -404,6 +334,9 @@ $(document).ready(function () {
 
   var timeDelay = 3900;
   setTimeout(zomatoAPI, timeDelay);
+
+  var timeDelay = 500;
+  setTimeout(tripAd, timeDelay);
 
 
 });
