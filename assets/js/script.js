8c5268ba547b46bb2634cc69e0ddcd56618056d0
Ed Sloane <edsloanephoto@gmail.com>
2020-06-24 20:56:52 +1000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -158,7 +158,7 @@ $(document).ready(function () {
     var latlong = latitude + "," + longitude;
     console.log(latlong)
     var ticketMasterURL =
-      "https://app.ticketmaster.com/discovery/v2/events.json?size=20&dmaId=701&apikey=" +
+      "https://app.ticketmaster.com/discovery/v2/events.json?&size=20&apikey=" +
       apiTicketmaster +
       "&" +
       latlong;
